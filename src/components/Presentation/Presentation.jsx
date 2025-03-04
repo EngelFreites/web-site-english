5a436c2cdb67372d8ecbcad920901a2d1e6abc26
Engelbert <engelfreites97@gmail.com>
2025-03-04 12:30:03 -0500

    freat(presentation): create presentation component

---
+++ src/components/Presentation/Presentation.jsx
@@ -0,0 +1,17 @@
+import "./presentation.css";
+export default function Presentation() {
+  return (
+    <div className="col-6 presentation">
+      <h1>
+        <span className="highlight">
+          🚀 Domina el inglés y abre un mundo de oportunidades 🌍
+        </span>
+      </h1>
+      <h2>
+        Aprende de manera rápida, divertida y efectiva. ¡Comienza hoy y
+        transforma tu futuro!
+      </h2>
+      <button className="start-button">Start Now</button>
+    </div>
+  );
+}
